1ea43ae5220ec95a20e25d666493dcee5ee43861
sigmabeta <bastos.eder@gmail.com>
2019-09-26 10:13:23 -0400

    Componentize Part selector & remove old adapter classes

--- ui-components/src/main/java/com/vgleadsheets/components/SheetListModel.kt
+++ ui-components/src/main/java/com/vgleadsheets/components/SheetListModel.kt
@@ -1,15 +1,12 @@
 package com.vgleadsheets.components
 
-import androidx.databinding.BaseObservable
-
 data class SheetListModel(
     val imageUrl: String,
     val status: Status,
     val listener: ImageListener,
-    val type: String = TYPE_DEFAULT,
     override val dataId: Long = imageUrl.hashCode().toLong(),
     override val layoutId: Int = R.layout.list_component_sheet
-) : BaseObservable(), ListModel {
+) : ListModel {
     interface ImageListener {
         fun onClicked(clicked: SheetListModel)
         fun onLoadStart(imageUrl: String)
@@ -22,9 +19,5 @@ data class SheetListModel(
         LOADING,
         ERROR,
         SUCCESS
-    }
-
-    companion object {
-        val TYPE_DEFAULT = SheetListModel::class.java.name
     }
 }
