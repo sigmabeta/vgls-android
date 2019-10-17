e72a43d98d0bd347c5d43f94b5f0df420dd4f410
sigmabeta <bastos.eder@gmail.com>
2019-10-17 15:09:05 -0400

    Static analysis

--- tracking-noop/src/main/java/com/vgleadsheets/tracking/NoopTracker.kt
+++ tracking-noop/src/main/java/com/vgleadsheets/tracking/NoopTracker.kt
@@ -3,9 +3,9 @@ package com.vgleadsheets.tracking
 import android.app.Activity
 
 class NoopTracker : Tracker {
-    override fun logScreenView(activity: Activity, screenName: String)= Unit
-    override fun logMenuShow()= Unit
-    override fun logForceRefresh()= Unit
-    override fun logSearch(query: String)= Unit
-    override fun logPartClicked(name: String)= Unit
+    override fun logScreenView(activity: Activity, screenName: String) = Unit
+    override fun logMenuShow() = Unit
+    override fun logForceRefresh() = Unit
+    override fun logSearch(query: String) = Unit
+    override fun logPartClicked(name: String) = Unit
 }
