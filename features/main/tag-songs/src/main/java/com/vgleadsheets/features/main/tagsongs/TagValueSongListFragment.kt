8b1e1161fc257f46d10bfa976b05f2b48eec9674
Eder Bastos <bastos.eder@gmail.com>
2020-06-16 09:22:55 -0400

    Convert tag value song list screen

--- features/main/tag-songs/src/main/java/com/vgleadsheets/features/main/tagsongs/TagValueSongListFragment.kt
+++ features/main/tag-songs/src/main/java/com/vgleadsheets/features/main/tagsongs/TagValueSongListFragment.kt
@@ -1,192 +1,53 @@
 package com.vgleadsheets.features.main.tagsongs
 
 import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.airbnb.mvrx.Async
-import com.airbnb.mvrx.Fail
-import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.MvRx
-import com.airbnb.mvrx.Success
-import com.airbnb.mvrx.Uninitialized
-import com.airbnb.mvrx.existingViewModel
+import com.airbnb.mvrx.args
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
-import com.vgleadsheets.VglsFragment
 import com.vgleadsheets.args.IdArgs
-import com.vgleadsheets.components.EmptyStateListModel
-import com.vgleadsheets.components.ErrorStateListModel
-import com.vgleadsheets.components.ImageNameCaptionListModel
-import com.vgleadsheets.components.ListModel
-import com.vgleadsheets.components.LoadingNameCaptionListModel
-import com.vgleadsheets.components.TitleListModel
-import com.vgleadsheets.features.main.hud.HudViewModel
-import com.vgleadsheets.features.main.hud.parts.PartSelectorItem
-import com.vgleadsheets.model.song.Song
-import com.vgleadsheets.model.tag.TagValue
-import com.vgleadsheets.recyclerview.ComponentAdapter
-import com.vgleadsheets.setInsetListenerForPadding
-import kotlinx.android.synthetic.main.fragment_tag_value_song_list.list_content
+import com.vgleadsheets.features.main.list.async.AsyncListFragment
 import javax.inject.Inject
 
-@Suppress("TooManyFunctions")
-class TagValueSongListFragment : VglsFragment(),
-    ImageNameCaptionListModel.EventHandler {
-
+class TagValueSongListFragment : AsyncListFragment<TagValueSongListData, TagValueSongListState>() {
     @Inject
     lateinit var tagValueViewModelFactory: TagValueSongListViewModel.Factory
 
-    private val viewModel: TagValueSongListViewModel by fragmentViewModel()
+    override val viewModel: TagValueSongListViewModel by fragmentViewModel()
 
-    private val hudViewModel: HudViewModel by existingViewModel()
+    private val idArgs: IdArgs by args()
 
-    private val adapter = ComponentAdapter()
+    override fun getVglsFragmentTag() = this.javaClass.simpleName + ":${idArgs.id}"
 
-    override fun onClicked(clicked: ImageNameCaptionListModel) {
-        showSongViewer(clicked.dataId)
-    }
+    override fun subscribeToViewEvents() {
+        viewModel.selectSubscribe(TagValueSongListState::clickedListModel) {
+            val clickedId = it?.dataId
 
-    override fun clearClicked() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val topOffset = resources.getDimension(R.dimen.height_search_bar).toInt() +
-                resources.getDimension(R.dimen.margin_large).toInt()
-        val bottomOffset = resources.getDimension(R.dimen.height_bottom_sheet_peek).toInt() +
-                resources.getDimension(R.dimen.margin_medium).toInt()
-
-        list_content.adapter = adapter
-        list_content.layoutManager = LinearLayoutManager(context)
-        list_content.setInsetListenerForPadding(
-            topOffset = topOffset,
-            bottomOffset = bottomOffset
-        )
-    }
-
-    override fun invalidate() = withState(hudViewModel, viewModel) { hudState, state ->
-        hudViewModel.alwaysShowBack()
-
-        val selectedPart = hudState.parts.first { it.selected }
-
-        val listModels = constructList(state.tagValue, state.songs, selectedPart)
-        adapter.submitList(listModels)
-    }
-
-    private fun constructList(
-        tagValue: Async<TagValue>,
-        songs: Async<List<Song>>,
-        selectedPart: PartSelectorItem
-    ): List<ListModel> {
-        if (tagValue !is Success) return emptyList()
-
-        val songListModels = createContentListModels(songs, selectedPart)
-
-        // Pass in `songs` so we know whether to show a sheet counter or not.
-        val titleListModel =
-            arrayListOf(createTitleListModel(tagValue(), songs, songListModels.size))
-
-        return titleListModel + songListModels
-    }
-
-    private fun createTitleListModel(
-        tagValue: TagValue,
-        songs: Async<List<Song>>,
-        songCount: Int
-    ) = TitleListModel(
-        R.string.title.toLong(),
-        getString(R.string.title_tag_value_songs, tagValue.tagKeyName, tagValue.name),
-        generateSheetCountText(songs, songCount)
-    )
-
-    private fun generateSheetCountText(
-        songs: Async<List<Song>>,
-        songCount: Int
-    ) = if (songs is Success) getString(R.string.subtitle_sheets_count, songCount) else ""
-
-    private fun createContentListModels(
-        songs: Async<List<Song>>,
-        selectedPart: PartSelectorItem
-    ) = when (songs) {
-        is Loading, Uninitialized -> createLoadingListModels()
-        is Fail -> createErrorStateListModel(songs.error)
-        is Success -> createSuccessListModels(songs(), selectedPart)
-    }
-
-    private fun createLoadingListModels(): List<ListModel> {
-        val listModels = ArrayList<ListModel>(LOADING_ITEMS)
-
-        for (index in 0 until LOADING_ITEMS) {
-            listModels.add(
-                LoadingNameCaptionListModel(index)
-            )
-        }
-
-        return listModels
-    }
-
-    private fun createErrorStateListModel(error: Throwable) =
-        listOf(ErrorStateListModel(error.message ?: "Unknown Error"))
-
-    private fun createSuccessListModels(
-        songs: List<Song>,
-        selectedPart: PartSelectorItem
-    ) = if (songs.isEmpty()) {
-        arrayListOf(
-            EmptyStateListModel(
-                R.drawable.ic_album_24dp,
-                "No songs found at all. Check your internet connection?"
-            )
-        )
-    } else {
-        val availableSongs = filterSongs(songs, selectedPart)
-
-        if (availableSongs.isEmpty()) {
-            arrayListOf(
-                EmptyStateListModel(
-                    R.drawable.ic_album_24dp,
-                    "No songs found with a ${selectedPart.apiId} part. Try another part?"
-                )
-            )
-        } else {
-            availableSongs.map {
-                val thumbUrl = it
-                    .parts
-                    ?.first { part -> part.name == selectedPart.apiId }
-                    ?.pages
-                    ?.first()
-                    ?.imageUrl
-
-                ImageNameCaptionListModel(
-                    it.id,
-                    it.name,
-                    it.gameName,
-                    thumbUrl,
-                    R.drawable.placeholder_sheet,
-                    this
-                )
+            if (clickedId != null) {
+                showSongViewer(clickedId)
+                viewModel.clearClicked()
             }
         }
     }
 
-    private fun filterSongs(
-        songs: List<Song>,
-        selectedPart: PartSelectorItem
-    ) = songs.filter { song ->
-        song.parts?.firstOrNull { part -> part.name == selectedPart.apiId } != null
-    }
+    private fun showSongViewer(clickedSongId: Long) =
+        withState(hudViewModel, viewModel) { hudState, state ->
+            val song = state.data.songs()?.first { it.id == clickedSongId }
 
-    private fun showSongViewer(songId: Long) {
-        getFragmentRouter().showSongViewer(songId)
-    }
-
-    override fun getLayoutId() = R.layout.fragment_tag_value_song_list
-
-    override fun getVglsFragmentTag() = this.javaClass.simpleName
+            if (song == null) {
+                showError("Failed to show song.")
+                return@withState
+            }
+            tracker.logSongView(
+                song.name,
+                song.gameName,
+                hudState.parts.first { it.selected }.apiId,
+                null
+            )
+            getFragmentRouter().showSongViewer(clickedSongId)
+        }
 
     companion object {
-        const val LOADING_ITEMS = 15
         fun newInstance(idArgs: IdArgs): TagValueSongListFragment {
             val fragment = TagValueSongListFragment()
 
--- features/main/tag-songs/src/main/java/com/vgleadsheets/features/main/tagsongs/TagValueSongListViewModel.kt
+++ features/main/tag-songs/src/main/java/com/vgleadsheets/features/main/tagsongs/TagValueSongListViewModel.kt
@@ -1,34 +1,169 @@
 package com.vgleadsheets.features.main.tagsongs
 
+import com.airbnb.mvrx.Async
+import com.airbnb.mvrx.Fail
 import com.airbnb.mvrx.FragmentViewModelContext
+import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.MvRxViewModelFactory
+import com.airbnb.mvrx.Success
+import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.ViewModelContext
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
-import com.vgleadsheets.mvrx.MvRxViewModel
+import com.vgleadsheets.components.EmptyStateListModel
+import com.vgleadsheets.components.ImageNameCaptionListModel
+import com.vgleadsheets.components.ListModel
+import com.vgleadsheets.components.TitleListModel
+import com.vgleadsheets.features.main.hud.parts.PartSelectorItem
+import com.vgleadsheets.features.main.list.async.AsyncListViewModel
+import com.vgleadsheets.model.song.Song
+import com.vgleadsheets.model.tag.TagValue
 import com.vgleadsheets.repository.Repository
+import com.vgleadsheets.resources.ResourceProvider
 
 class TagValueSongListViewModel @AssistedInject constructor(
     @Assisted initialState: TagValueSongListState,
-    private val repository: Repository
-) : MvRxViewModel<TagValueSongListState>(initialState) {
+    private val repository: Repository,
+    private val resourceProvider: ResourceProvider
+) : AsyncListViewModel<TagValueSongListData, TagValueSongListState>(initialState, resourceProvider),
+    ImageNameCaptionListModel.EventHandler {
     init {
         fetchTagValue()
         fetchSongs()
     }
 
+    override fun onClicked(clicked: ImageNameCaptionListModel) = setState {
+        copy(
+            clickedListModel = clicked
+        )
+    }
+
+    override fun clearClicked() = setState {
+        copy(
+            clickedListModel = null
+        )
+    }
+
+    override fun createTitleListModel(): TitleListModel? = null
+
+    override fun createFullEmptyStateListModel() = EmptyStateListModel(
+        R.drawable.ic_album_24dp,
+        "No songs found at all. Check your internet connection?"
+    )
+
+    override fun createSuccessListModels(
+        data: TagValueSongListData,
+        updateTime: Async<*>,
+        digest: Async<*>,
+        selectedPart: PartSelectorItem
+    ): List<ListModel> {
+        val title = createTitleListModel(data.tagValue, data.songs)
+        val content = createContentListModels(data.songs, selectedPart)
+
+        return title + content
+    }
+
     private fun fetchTagValue() = withState { state ->
-        repository.getTagValue(state.tagValueId)
-            .execute { data ->
-                copy(tagValue = data)
+        repository
+            .getTagValue(state.tagValueId)
+            .execute { tagValue ->
+                val newData = TagValueSongListData(tagValue, data.songs)
+                updateListState(
+                    data = newData,
+                    listModels = constructList(
+                        newData,
+                        this
+                    )
+                )
             }
     }
 
     private fun fetchSongs() = withState { state ->
-        repository.getSongsForTagValue(state.tagValueId)
-            .execute { data ->
-                copy(songs = data)
+        repository
+            .getSongsForTagValue(state.tagValueId)
+            .execute { songs ->
+                val newData = TagValueSongListData(data.tagValue, songs)
+                updateListState(
+                    data = newData,
+                    listModels = constructList(
+                        newData,
+                        this
+                    )
+                )
             }
+    }
+
+    private fun createTitleListModel(
+        tagValue: Async<TagValue>,
+        songs: Async<List<Song>>
+    ): List<ListModel> =
+        when (tagValue) {
+            is Success -> listOf(
+                TitleListModel(
+                    tagValue().id,
+                    resourceProvider.getString(R.string.title_tag_value_songs, tagValue().tagKeyName, tagValue().name),
+                    generateSheetCountText(songs)
+                )
+            )
+            is Fail -> createErrorStateListModel(tagValue.error)
+            is Uninitialized, is Loading -> createLoadingListModels()
+            else -> createErrorStateListModel(IllegalStateException("Unhandled title state."))
+        }
+
+    private fun generateSheetCountText(
+        songs: Async<List<Song>>
+    ) = if (songs is Success) {
+        resourceProvider.getString(R.string.subtitle_sheets_count, songs().size)
+    } else ""
+
+    private fun createContentListModels(
+        songs: Async<List<Song>>,
+        selectedPart: PartSelectorItem
+    ) = when (songs) {
+        is Success -> createSongListModels(songs(), selectedPart)
+        is Fail -> createErrorStateListModel(songs.error)
+        is Uninitialized, is Loading -> createLoadingListModels()
+    }
+
+    private fun createSongListModels(
+        songs: List<Song>,
+        selectedPart: PartSelectorItem
+    ): List<ListModel> {
+        val availableSongs = filterSongs(songs, selectedPart)
+
+        return if (availableSongs.isEmpty()) {
+            arrayListOf(
+                EmptyStateListModel(
+                    R.drawable.ic_album_24dp,
+                    "No songs found with a ${selectedPart.apiId} part. Try another part?"
+                )
+            )
+        } else {
+            availableSongs.map {
+                val thumbUrl = it
+                    .parts
+                    ?.first { part -> part.name == selectedPart.apiId }
+                    ?.pages
+                    ?.first()
+                    ?.imageUrl
+
+                ImageNameCaptionListModel(
+                    it.id,
+                    it.name,
+                    it.gameName,
+                    thumbUrl,
+                    R.drawable.placeholder_sheet,
+                    this@TagValueSongListViewModel
+                )
+            }
+        }
+    }
+
+    private fun filterSongs(
+        songs: List<Song>,
+        selectedPart: PartSelectorItem
+    ) = songs.filter { song ->
+        song.parts?.firstOrNull { part -> part.name == selectedPart.apiId } != null
     }
 
     @AssistedInject.Factory
@@ -47,3 +182,4 @@ class TagValueSongListViewModel @AssistedInject constructor(
         }
     }
 }
+
