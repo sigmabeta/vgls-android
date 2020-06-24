191e279920722cc763544e0dbc15aab318cba3b5
Eder Bastos <bastos.eder@gmail.com>
2020-06-24 09:39:59 -0400

    Add beta API support

--- common/debug-common/src/main/java/com/vgleadsheets/common/debug/NetworkEndpoint.kt
+++ common/debug-common/src/main/java/com/vgleadsheets/common/debug/NetworkEndpoint.kt
@@ -6,5 +6,6 @@ enum class NetworkEndpoint(
 ) {
     PROD(R.string.network_endpoint_prod, "https://www.vgleadsheets.com/"),
     SUPER(R.string.network_endpoint_super, "https://super.vgleadsheets.com/"),
+    BETA(R.string.network_endpoint_beta, "https://beta.vgleadsheets.com/"),
     MOCK(R.string.network_endpoint_mock, null),
 }
