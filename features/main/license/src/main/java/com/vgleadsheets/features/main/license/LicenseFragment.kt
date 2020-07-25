8228e583109288adba673aa23eb051a7afe6b44a
Eder Bastos <bastos.eder@gmail.com>
2020-07-25 19:42:45 -0400

    Disable perf tracking on License screen

--- features/main/license/src/main/java/com/vgleadsheets/features/main/license/LicenseFragment.kt
+++ features/main/license/src/main/java/com/vgleadsheets/features/main/license/LicenseFragment.kt
@@ -63,6 +63,8 @@ class LicenseFragment : VglsFragment() {
 
     override fun getTrackingScreen() = TrackingScreen.LICENSE
 
+    override fun disablePerfTracking() = true
+
     companion object {
         fun newInstance() = LicenseFragment()
     }
