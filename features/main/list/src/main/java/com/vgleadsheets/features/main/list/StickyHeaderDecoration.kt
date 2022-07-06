a24b79c83270f619f4f3da5f0985540c9d6598fa
Eder Bastos <bastos.eder@gmail.com>
2022-07-06 12:25:49 -0400

    Tweaks to sticky header

--- features/main/list/src/main/java/com/vgleadsheets/features/main/list/StickyHeaderDecoration.kt
+++ features/main/list/src/main/java/com/vgleadsheets/features/main/list/StickyHeaderDecoration.kt
@@ -4,38 +4,27 @@ import android.graphics.Canvas
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.MeasureSpec
-import android.view.View.VISIBLE
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import androidx.recyclerview.widget.RecyclerView.State
 import com.vgleadsheets.components.ComponentViewHolder
+import com.vgleadsheets.components.ListModel
 import com.vgleadsheets.components.LoadingTitleListModel
 import com.vgleadsheets.components.TitleListModel
 import com.vgleadsheets.recyclerview.ComponentAdapter
 
 class StickyHeaderDecoration(
     private val adapter: ComponentAdapter,
-    root: View
+    private val root: View
 ) : ItemDecoration() {
-    private val headerViewHolder by lazy {
-        val binding = DataBindingUtil.inflate<ViewDataBinding>(
-            LayoutInflater.from(root.context),
-            R.layout.list_component_title,
-            null,
-            false
-        )
+    private var previousLayoutId: Int = 0
 
-        binding.root.visibility = VISIBLE
+    private var previousViewHolder: ComponentViewHolder? = null
 
-        ComponentViewHolder(
-            binding
-        )
-    }
-
-    private val stickyHeader: View
-        get() = headerViewHolder.itemView
+    private val stickyHeader: View?
+        get() = previousViewHolder?.itemView
 
     override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: State) {
         super.onDrawOver(canvas, parent, state)
@@ -48,7 +37,9 @@ class StickyHeaderDecoration(
         }
 
         // Don't render if loading views are at the top.
-        if (scrollingHeader.id == com.vgleadsheets.ui_core.R.id.component_loading_name_caption) {
+        if (headerModel !is LoadingTitleListModel &&
+            scrollingHeader.id == com.vgleadsheets.ui_core.R.id.component_loading_name_caption
+        ) {
             return
         }
 
@@ -59,28 +50,73 @@ class StickyHeaderDecoration(
             1.0f
         }
 
-        val modelDupe = (headerModel as TitleListModel).copy(
-            shrinkPercent = progress,
-            hidden = progress <= 0.0f,
-            shouldSetMinHeightOnly = true
-        )
-        headerViewHolder.bind(modelDupe)
+        val modelDupe = getDupedModel(headerModel, progress)
+        val viewHolder = getCorrectViewHolder(modelDupe)
+
+        viewHolder.bind(modelDupe)
 
         layoutHeaderView(scrollingHeader)
         canvas.drawHeaderView()
     }
 
-    private fun layoutHeaderView(scrollingView: View) {
-        stickyHeader.measure(
-            MeasureSpec.makeMeasureSpec(scrollingView.width, MeasureSpec.EXACTLY),
-            MeasureSpec.makeMeasureSpec(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+    private fun getCorrectViewHolder(modelDupe: ListModel): ComponentViewHolder {
+        val oldHolder = previousViewHolder
+        return if (previousLayoutId == modelDupe.layoutId && oldHolder != null) {
+            oldHolder
+        } else {
+            val binding = DataBindingUtil.inflate<ViewDataBinding>(
+                LayoutInflater.from(root.context),
+                modelDupe.layoutId,
+                null,
+                false
+            )
+
+            binding.root.visibility = View.VISIBLE
+
+            val newHolder = ComponentViewHolder(
+                binding
+            )
+
+            previousViewHolder = newHolder
+            newHolder
+        }
+    }
+
+    private fun getDupedModel(
+        headerModel: ListModel?,
+        progress: Float
+    ) = when (headerModel) {
+        is TitleListModel -> headerModel.copy(
+            shrinkPercent = progress,
+            hidden = progress <= 0.0f,
+            shouldSetMinHeightOnly = true
         )
-        stickyHeader.layout(scrollingView.left, 0, scrollingView.right, stickyHeader.measuredHeight)
+        is LoadingTitleListModel -> headerModel.copy(
+            shrinkPercent = progress,
+            hidden = progress <= 0.0f,
+            shouldSetMinHeightOnly = true
+        )
+        else -> throw IllegalArgumentException("HeaderModel needs to be some sort of title.")
+    }
+
+    private fun layoutHeaderView(scrollingView: View) {
+        stickyHeader?.let {
+            it.measure(
+                MeasureSpec.makeMeasureSpec(scrollingView.width, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+            )
+            it.layout(
+                scrollingView.left,
+                0,
+                scrollingView.right,
+                it.measuredHeight
+            )
+        }
     }
 
     private fun Canvas.drawHeaderView() {
         save()
-        stickyHeader.draw(this)
+        stickyHeader?.draw(this)
         restore()
     }
 }
