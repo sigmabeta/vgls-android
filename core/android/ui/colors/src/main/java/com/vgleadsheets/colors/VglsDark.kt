124bf6001a42a1051d263b8068193ebc3260570e
Eder Bastos <bastos.eder@gmail.com>
2022-12-04 15:09:09 -0500

    WIP One component in compose...nent

---
+++ core/android/ui/colors/src/main/java/com/vgleadsheets/colors/VglsDark.kt
@@ -0,0 +1,67 @@
+package com.vgleadsheets.colors
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color
+
+val md_theme_dark_primary = Color(0xFFEBB2FF)
+val md_theme_dark_onPrimary = Color(0xFF520072)
+val md_theme_dark_primaryContainer = Color(0xFF721099)
+val md_theme_dark_onPrimaryContainer = Color(0xFFF8D8FF)
+val md_theme_dark_secondary = Color(0xFFFFABEF)
+val md_theme_dark_onSecondary = Color(0xFF5B0557)
+val md_theme_dark_secondaryContainer = Color(0xFF76246F)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFD7F3)
+val md_theme_dark_tertiary = Color(0xFFA5C8FF)
+val md_theme_dark_onTertiary = Color(0xFF00315F)
+val md_theme_dark_tertiaryContainer = Color(0xFF004786)
+val md_theme_dark_onTertiaryContainer = Color(0xFFD4E3FF)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF1E1B1E)
+val md_theme_dark_onBackground = Color(0xFFE8E0E5)
+val md_theme_dark_surface = Color(0xFF1E1B1E)
+val md_theme_dark_onSurface = Color(0xFFE8E0E5)
+val md_theme_dark_surfaceVariant = Color(0xFF4C444D)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCEC3CD)
+val md_theme_dark_outline = Color(0xFF978E97)
+val md_theme_dark_inverseOnSurface = Color(0xFF1E1B1E)
+val md_theme_dark_inverseSurface = Color(0xFFE8E0E5)
+val md_theme_dark_inversePrimary = Color(0xFF8C33B3)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFEBB2FF)
+val md_theme_dark_outlineVariant = Color(0xFF4C444D)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+val VglsDark = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
+)
