c0cf3ba33237f456970ea56d6b2801bdff31a117
sigmabeta <bastos.eder@gmail.com>
2022-12-04 20:41:36 -0500

    Implement ImageNameCaptionListItem in compose

--- core/android/ui-components/src/main/java/com/vgleadsheets/composables/ImageNameCaptionListItem.kt
+++ core/android/ui-components/src/main/java/com/vgleadsheets/composables/ImageNameCaptionListItem.kt
@@ -2,44 +2,95 @@ package com.vgleadsheets.composables
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.vgleadsheets.components.R
+import com.vgleadsheets.images.previewPlaceholder
 import com.vgleadsheets.themes.VglsMaterial
 
 @Composable
 fun ImageNameCaptionListItem(
-//    dataId: Long,
     name: String,
     caption: String,
-//    imageUrl: String?,
-//    @DrawableRes imagePlaceholder: Int,
-//    actionableId: Long? = null,
-//    onClick: () -> Unit
+    imageUrl: String?,
+    @DrawableRes imagePlaceholder: Int,
+    onClick: () -> Unit
 ) {
-    Column(
+    Row(
         modifier = Modifier
             .padding(all = 8.dp)
             .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable(onClick = onClick)
     ) {
-        Text(
-            text = name,
-            style = MaterialTheme.typography.bodyMedium,
-            maxLines = 1
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = null,
+            placeholder = previewPlaceholder(
+                previewPlaceholderId = R.drawable.img_preview_game,
+                realPlaceholderId = imagePlaceholder
+            ),
+            modifier = Modifier
+                .width(48.dp)
+                .aspectRatio(1.0f)
+                .clip(CircleShape)
         )
 
-        Text(
-            text = caption,
-            style = MaterialTheme.typography.labelSmall,
-            maxLines = 1
+        Spacer(
+            modifier = Modifier.width(8.dp)
         )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
+            Spacer(
+                modifier = Modifier.height(4.dp)
+            )
+
+            Text(
+                text = name,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(
+                modifier = Modifier.height(4.dp)
+            )
+
+            Text(
+                text = caption,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onBackground,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 }
 
@@ -74,12 +125,10 @@ fun Dark() {
 @Composable
 private fun Sample() {
     ImageNameCaptionListItem(
-//        -1L,
-        "Chrono Trigger",
-        "Yasunori Mitsuda",
-//        "",
-//        0,
-//        null,
-//        {}
+        "Xenoblade Chronicles 3",
+        "Yasunori Mitsuda, Mariam Abounnasr, Manami Kiyota, ACE+, Kenji Hiramatsu",
+        "https://randomfox.ca/images/12.jpg",
+        R.drawable.ic_album_24dp,
+        {}
     )
 }
