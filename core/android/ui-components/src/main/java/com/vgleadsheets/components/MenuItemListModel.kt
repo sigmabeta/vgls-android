ab0f54dfb2f95921a47e7675366bbcbbffb9cefd
sigmabeta <bastos.eder@gmail.com>
2022-12-21 09:46:51 -0500

    Implement Menu List Item composable

--- core/android/ui-components/src/main/java/com/vgleadsheets/components/MenuItemListModel.kt
+++ core/android/ui-components/src/main/java/com/vgleadsheets/components/MenuItemListModel.kt
@@ -1,13 +1,20 @@
 package com.vgleadsheets.components
 
+import androidx.compose.runtime.Composable
+import com.vgleadsheets.composables.MenuItem
+
 data class MenuItemListModel(
     val name: String,
     val caption: String?,
     val iconId: Int,
     val onClick: () -> Unit,
     val selected: Boolean = false
-) : ListModel {
+) : ListModel, ComposableModel {
     override val dataId = name.hashCode().toLong()
+    override val layoutId = R.layout.composable_list_item
 
-    override val layoutId = R.layout.list_component_menu_item
+    @Composable
+    override fun Content() {
+        MenuItem(model = this)
+    }
 }
---
+++ core/android/ui-components/src/main/java/com/vgleadsheets/composables/MenuItem.kt
@@ -0,0 +1,152 @@
+package com.vgleadsheets.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.DeviceFontFamilyName
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vgleadsheets.components.MenuItemListModel
+import com.vgleadsheets.components.R
+import com.vgleadsheets.themes.VglsMaterialMenu
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun MenuItem(
+    model: MenuItemListModel
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(
+                horizontal = dimensionResource(id = R.dimen.margin_small)
+            )
+            .clickable(
+                onClick = model.onClick,
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        val color = if (model.selected) {
+            MaterialTheme.colorScheme.onTertiaryContainer
+        } else {
+            MaterialTheme.colorScheme.onPrimary
+        }
+
+        Icon(
+            painterResource(
+                id = model.iconId,
+            ),
+            tint = color,
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 4.dp)
+                .padding(vertical = 12.dp)
+        )
+
+        Spacer(
+            modifier = Modifier.width(12.dp)
+        )
+
+        Text(
+            text = model.name,
+            style = MaterialTheme.typography.bodyMedium,
+            color = color,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = if (model.selected) FontWeight.Bold else FontWeight.Normal,
+            modifier = Modifier
+                .padding(
+                    top = 16.dp,
+                    bottom = 16.dp
+                )
+        )
+
+        Text(
+            text = model.caption ?: "",
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.bodyMedium.copy(
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily(
+                    Font(DeviceFontFamilyName("sans-serif-condensed")),
+                )
+            ),
+            color = color,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 16.dp,
+                    bottom = 16.dp,
+                    end = 8.dp
+                )
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun Selected() {
+    VglsMaterialMenu {
+        Box(
+            modifier = Modifier.background(
+                color = MaterialTheme.colorScheme.background
+            )
+        ) {
+            Sample(true)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NotSelected() {
+    VglsMaterialMenu {
+        Box(
+            modifier = Modifier.background(
+                color = MaterialTheme.colorScheme.background
+            )
+        ) {
+            Sample(false)
+        }
+    }
+}
+
+@Composable
+private fun Sample(selected: Boolean) {
+    MenuItem(
+        MenuItemListModel(
+            "Check for updates...",
+            "Last updated Feb 3, 1963",
+            R.drawable.ic_refresh_24dp,
+            {},
+            selected
+        )
+    )
+}
+
