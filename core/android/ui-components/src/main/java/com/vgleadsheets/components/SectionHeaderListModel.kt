b4d0740e5fdc30a387daff1aac7c3bf8c6ec3604
Eder Bastos <bastos.eder@gmail.com>
2022-12-25 20:37:21 -0500

    Implement section header in compose

--- core/android/ui-components/src/main/java/com/vgleadsheets/components/SectionHeaderListModel.kt
+++ core/android/ui-components/src/main/java/com/vgleadsheets/components/SectionHeaderListModel.kt
@@ -1,8 +1,16 @@
 package com.vgleadsheets.components
 
+import androidx.compose.runtime.Composable
+import com.vgleadsheets.composables.SectionHeader
+
 data class SectionHeaderListModel(
     val title: String
-) : ListModel {
+) : ListModel, ComposableModel {
     override val dataId = title.hashCode().toLong()
-    override val layoutId = R.layout.list_component_section_header
+    override val layoutId = R.layout.composable_menu_item
+
+    @Composable
+    override fun Content() {
+        SectionHeader(name = title, menu = true)
+    }
 }
--- features/main/search/src/main/java/com/vgleadsheets/features/main/search/Config.kt
+++ features/main/search/src/main/java/com/vgleadsheets/features/main/search/Config.kt
@@ -150,7 +150,7 @@ class Config(
         }
     }
 
-    private fun createSectionHeaderListModel(sectionId: Int) =
+    private fun createSectionHeaderListModel(sectionId: Int): List<ListModel> =
         listOf(SectionHeaderListModel(resources.getString(sectionId)))
 
     private suspend fun createSectionModels(
