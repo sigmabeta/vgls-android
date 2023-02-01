9ae819423ff267a65e251ac581d5e39152b6c559
Eder Bastos <bastos.eder@gmail.com>
2023-02-01 18:58:22 -0500

    Convert Cta to compose

--- core/android/ui-components/src/main/java/com/vgleadsheets/components/CtaListModel.kt
+++ core/android/ui-components/src/main/java/com/vgleadsheets/components/CtaListModel.kt
@@ -1,11 +1,22 @@
 package com.vgleadsheets.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.vgleadsheets.composables.ActionItem
+
 data class CtaListModel(
     val iconId: Int,
     val name: String,
     val onClick: () -> Unit,
-) : ListModel {
-    override val dataId = iconId.toLong()
+) : ListModel, ComposableModel {
+    override val dataId = name.hashCode().toLong()
+    override val layoutId = R.layout.composable_list_item
 
-    override val layoutId = R.layout.list_component_cta
+    @Composable
+    override fun Content(modifier: Modifier) {
+        ActionItem(
+            model = this,
+            modifier = modifier
+        )
+    }
 }
