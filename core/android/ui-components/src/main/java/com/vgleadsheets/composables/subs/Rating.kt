7eb768abeecd5b031f737f53123da5ea5ea8189d
Eder Bastos <bastos.eder@gmail.com>
2023-02-03 20:02:23 -0500

    Convert LabelRatingListItem to compose

---
+++ core/android/ui-components/src/main/java/com/vgleadsheets/composables/subs/Rating.kt
@@ -0,0 +1,89 @@
+package com.vgleadsheets.composables.subs
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.vgleadsheets.components.R
+import com.vgleadsheets.themes.VglsMaterial
+import com.vgleadsheets.themes.VglsMaterialMenu
+
+@Composable
+fun Rating(
+    score: Int,
+    modifier: Modifier,
+) {
+    Row(
+        modifier = modifier
+    ) {
+        for (index in 1..5) {
+            val iconId = if (score >= index) {
+                R.drawable.ic_jam_filled
+            } else {
+                R.drawable.ic_jam_unfilled
+            }
+
+            Icon(
+                painter = painterResource(id = iconId),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onTertiaryContainer,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Light() {
+    VglsMaterial {
+        Box(
+            modifier = Modifier.background(
+                color = MaterialTheme.colorScheme.background
+            )
+        ) {
+            Sample()
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun Dark() {
+    VglsMaterial {
+        Box(
+            modifier = Modifier.background(
+                color = MaterialTheme.colorScheme.background
+            )
+        ) {
+            Sample()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Menu() {
+    VglsMaterialMenu {
+        Box(
+            modifier = Modifier.background(
+                color = MaterialTheme.colorScheme.background
+            )
+        ) {
+            Sample()
+        }
+    }
+}
+
+@Composable
+private fun Sample() {
+    Rating(
+        3,
+        Modifier
+    )
+}
