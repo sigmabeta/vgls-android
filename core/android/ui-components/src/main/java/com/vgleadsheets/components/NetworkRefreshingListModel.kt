cb129b6882a1676057ff70cddaf5efe3154f5d54
Eder Bastos <bastos.eder@gmail.com>
2023-02-05 07:04:27 -0500

    Convert network refreshing item to compose, also it looked stupid

--- core/android/ui-components/src/main/java/com/vgleadsheets/components/NetworkRefreshingListModel.kt
+++ core/android/ui-components/src/main/java/com/vgleadsheets/components/NetworkRefreshingListModel.kt
@@ -1,9 +1,21 @@
 package com.vgleadsheets.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.vgleadsheets.composables.LoadingListItem
+
 data class NetworkRefreshingListModel(
     val refreshType: String
-) : ListModel {
+) : ListModel, ComposableModel {
     override val dataId = refreshType.hashCode().toLong()
+    override val layoutId = R.layout.composable_list_item
 
-    override val layoutId = R.layout.list_component_network_refreshing
+    @Composable
+    override fun Content(modifier: Modifier) {
+        LoadingListItem(
+            withImage = true,
+            seed = dataId,
+            modifier = modifier,
+        )
+    }
 }
