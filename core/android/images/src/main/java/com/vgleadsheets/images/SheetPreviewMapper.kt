afcff6d9fc5cbaa4404ac23f20473ba20306787c
a-rosen <annierrosen@gmail.com>
2024-06-01 11:36:04 -0400

    Remove unnecessary parentheses

--- core/android/images/src/main/java/com/vgleadsheets/images/SheetPreviewMapper.kt
+++ core/android/images/src/main/java/com/vgleadsheets/images/SheetPreviewMapper.kt
@@ -3,6 +3,6 @@ package com.vgleadsheets.images
 import coil.map.Mapper
 import coil.request.Options
 
-class SheetPreviewMapper() : Mapper<PagePreview, String> {
+class SheetPreviewMapper : Mapper<PagePreview, String> {
     override fun map(data: PagePreview, options: Options) = data.title
 }
