b465cae697e035042f2979dbd0a346190cac4865
a-rosen <annierrosen@gmail.com>
2024-06-02 10:50:14 -0400

    remove unused private properties (ComposerAliasAndroidDataSource), refactor thrown exception to require() (PdfToBitmapRenderer),

--- core/android/conversion/src/main/java/com/vgleadsheets/conversion/android/datasource/ComposerAliasAndroidDataSource.kt
+++ core/android/conversion/src/main/java/com/vgleadsheets/conversion/android/datasource/ComposerAliasAndroidDataSource.kt
@@ -2,10 +2,8 @@ package com.vgleadsheets.conversion.android.datasource
 
 import com.vgleadsheets.conversion.android.AndroidDataSource
 import com.vgleadsheets.conversion.android.converter.ComposerAliasConverter
-import com.vgleadsheets.conversion.android.converter.ComposerConverter
 import com.vgleadsheets.conversion.mapListTo
 import com.vgleadsheets.database.android.dao.ComposerAliasRoomDao
-import com.vgleadsheets.database.android.dao.ComposerRoomDao
 import com.vgleadsheets.database.android.enitity.ComposerAliasEntity
 import com.vgleadsheets.database.dao.ComposerAliasDataSource
 import com.vgleadsheets.model.alias.ComposerAlias
@@ -14,8 +12,6 @@ import com.vgleadsheets.model.alias.ComposerAlias
 class ComposerAliasAndroidDataSource(
     private val convert: ComposerAliasConverter,
     private val roomImpl: ComposerAliasRoomDao,
-    private val otoRelatedRoomImpl: ComposerRoomDao,
-    private val composerConverter: ComposerConverter,
 ) : AndroidDataSource<ComposerAliasRoomDao, ComposerAlias, ComposerAliasEntity, ComposerAliasConverter>(
     convert,
     roomImpl,
