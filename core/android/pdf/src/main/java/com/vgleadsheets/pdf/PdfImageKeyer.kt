e2d73f045554f9e26d62853e2fc200975e67afc1
sigmabeta <bastos.eder@gmail.com>
2024-06-08 18:54:36 -0400

    Ktlint

--- core/android/pdf/src/main/java/com/vgleadsheets/pdf/PdfImageKeyer.kt
+++ core/android/pdf/src/main/java/com/vgleadsheets/pdf/PdfImageKeyer.kt
@@ -4,7 +4,7 @@ import coil.key.Keyer
 import coil.request.Options
 import javax.inject.Inject
 
-class PdfImageKeyer @Inject constructor(): Keyer<PdfConfigById> {
+class PdfImageKeyer @Inject constructor() : Keyer<PdfConfigById> {
     override fun key(data: PdfConfigById, options: Options): String {
         val width = computeWidth(options)
         return data.cacheKey(width)
