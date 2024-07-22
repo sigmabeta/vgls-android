a7d67ffb29c7c597700e32228ae7217a7c2f8c95
Eder Bastos <bastos.eder@gmail.com>
2024-07-22 11:04:10 -0400

    Fix busted local release build

--- app/src/release/java/com/vgleadsheets/di/PerfBackendModule.kt
+++ app/src/release/java/com/vgleadsheets/di/PerfBackendModule.kt
@@ -1,29 +1,31 @@
 package com.vgleadsheets.di
 
-import com.google.firebase.perf.FirebasePerformance
-import com.vgleadsheets.perf.tracking.common.PerfTrackingBackend
-import com.vgleadsheets.perf.tracking.firebase.FirebasePerfBackend
-import com.vgleadsheets.tracking.Tracker
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+// TODO Move this file into the actual analytics  modules.
 
-@InstallIn(SingletonComponent::class)
-@Module
-object PerfBackendModule {
-    @Provides
-    @Singleton
-    fun provideFirebasePerfInstance(): FirebasePerformance = FirebasePerformance.getInstance()
-
-    @Provides
-    @Singleton
-    fun providePerfBackend(
-        firebase: FirebasePerformance,
-        tracker: Tracker
-    ): PerfTrackingBackend = FirebasePerfBackend(
-        firebase,
-        tracker
-    )
-}
+// import com.google.firebase.perf.FirebasePerformance
+// import com.vgleadsheets.perf.tracking.common.PerfTrackingBackend
+// import com.vgleadsheets.perf.tracking.firebase.FirebasePerfBackend
+// import com.vgleadsheets.tracking.Tracker
+// import dagger.Module
+// import dagger.Provides
+// import dagger.hilt.InstallIn
+// import dagger.hilt.components.SingletonComponent
+// import javax.inject.Singleton
+//
+// @InstallIn(SingletonComponent::class)
+// @Module
+// object PerfBackendModule {
+//     @Provides
+//     @Singleton
+//     fun provideFirebasePerfInstance(): FirebasePerformance = FirebasePerformance.getInstance()
+//
+//     @Provides
+//     @Singleton
+//     fun providePerfBackend(
+//         firebase: FirebasePerformance,
+//         tracker: Tracker
+//     ): PerfTrackingBackend = FirebasePerfBackend(
+//         firebase,
+//         tracker
+//     )
+// }
