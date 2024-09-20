64f51990eddc2824f06aef615a8c8fedfe881c58
Eder Bastos <bastos.eder@gmail.com>
2024-09-20 17:00:40 -0400

    Fix gooves

--- core/android/ui/previews/src/test/java/com/vgleadsheets/composables/previews/screens/lists/ComposerListScreenshots.kt
+++ core/android/ui/previews/src/test/java/com/vgleadsheets/composables/previews/screens/lists/ComposerListScreenshots.kt
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 
 @RunWith(Parameterized::class)
-class ComposerDetailScreenshots(
+class ComposerListScreenshots(
     private val deviceConfig: DeviceConfig,
     private val category: String,
     private val width: String,
