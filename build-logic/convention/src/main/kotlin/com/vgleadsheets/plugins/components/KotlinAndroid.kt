bdcedbdca36db8f08e3a72153dd495d00961320a
Eder Bastos <bastos.eder@gmail.com>
2024-09-25 19:52:29 -0400

    Revert: circleCI can't handle all these processors, cap'n

--- build-logic/convention/src/main/kotlin/com/vgleadsheets/plugins/components/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/vgleadsheets/plugins/components/KotlinAndroid.kt
@@ -21,7 +21,6 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.provideDelegate
@@ -93,10 +92,6 @@ private fun Project.configureKotlin() {
                 "-opt-in=kotlinx.coroutines.FlowPreview",
             )
         }
-    }
-
-    tasks.withType<Test> {
-        maxParallelForks = Runtime.getRuntime().availableProcessors()
     }
 }
 
