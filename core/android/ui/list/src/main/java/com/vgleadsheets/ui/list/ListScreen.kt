ca16ebf69325694e4bb50590490959a360d26602
Eder Bastos <bastos.eder@gmail.com>
2024-09-27 15:34:30 -0400

    Work around homescreen not starting at the top

--- core/android/ui/list/src/main/java/com/vgleadsheets/ui/list/ListScreen.kt
+++ core/android/ui/list/src/main/java/com/vgleadsheets/ui/list/ListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -30,7 +31,13 @@ fun ListScreen(
         }
     }
 
+    val listState = rememberLazyListState()
+
+    // To work around home screen not starting at the top (now we here)
+    LaunchedEffect(items) { listState.animateScrollToItem(0) }
+
     LazyColumn(
+        state = listState,
         contentPadding = PaddingValues(vertical = 16.dp),
         modifier = modifier.fillMaxSize()
     ) {
