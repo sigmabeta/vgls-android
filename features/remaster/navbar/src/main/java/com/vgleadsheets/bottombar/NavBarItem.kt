08b19b0557d4268c08f72e501277a67dc250ce64
Eder Bastos <bastos.eder@gmail.com>
2024-11-05 13:01:12 -0500

    Refactor: Add favorites to some nav bars

--- features/remaster/navbar/src/main/java/com/vgleadsheets/bottombar/NavBarItem.kt
+++ features/remaster/navbar/src/main/java/com/vgleadsheets/bottombar/NavBarItem.kt
@@ -1,17 +1,31 @@
 package com.vgleadsheets.bottombar
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.ui.graphics.vector.ImageVector
+import com.vgleadsheets.nav.Destination
+import com.vgleadsheets.ui.Icon
 
-enum class NavBarItem(
+sealed class NavBarItem(
     val route: String,
-    val icon: ImageVector,
+    val icon: Icon,
     val label: String
 ) {
-    Home("home", Icons.Default.Home, "Home"),
-    Browse("browse", Icons.AutoMirrored.Default.List, "Browse"),
-    Search("search", Icons.Default.Search, "Search"),
+    data object Home : NavBarItem(Destination.HOME.noArgs(), Icon.HOME, "Home")
+    data object Browse : NavBarItem(Destination.BROWSE.noArgs(), Icon.BROWSE, "Browse")
+    data object Favorites : NavBarItem(Destination.FAVORITES.noArgs(), Icon.FAVORITE, "Favorites")
+    data object Search : NavBarItem(Destination.SEARCH.noArgs(), Icon.SEARCH, "Search")
+
+    companion object {
+        val RAIL_ITEMS = listOf(
+            Home,
+            Browse,
+            Favorites,
+            Search,
+        )
+
+        val BOTTOM_BAR_ITEMS = listOf(
+            Home,
+            Browse,
+            Search,
+        )
+    }
 }
+
