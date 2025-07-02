8bdb425c5d9b5360b1c8de3c2214e1983467caf0
sigmabeta <bastos.eder@gmail.com>
2025-07-02 15:30:42 -0400

    Fix detekt

--- core/android/nav/src/main/java/com/vgleadsheets/nav/NavViewModel.kt
+++ core/android/nav/src/main/java/com/vgleadsheets/nav/NavViewModel.kt
@@ -263,7 +263,9 @@ class NavViewModel @Inject constructor(
     private fun printBackstackStatus(navBackStackEntries: List<NavBackStackEntry>) {
         hatchet.d("Nav backstack updated.")
         navBackStackEntries.forEach { entry ->
-            hatchet.v("Dest: ${entry.destination.route} State: ${entry.lifecycle.currentState} Args: ${entry.arguments}")
+            hatchet.v("Dest: ${entry.destination.route} " +
+                "State: ${entry.lifecycle.currentState} " +
+                "Args: ${entry.arguments}")
         }
     }
 
