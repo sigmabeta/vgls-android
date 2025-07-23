5df5a9de87f821a2311d2c40bee757cd0b9f0070
mezzocarattere <annierrosen@gmail.com>
2025-07-23 16:06:08 -0400

    Linting fixes (remove unnecessary comment/dependency, fix order of fields)

--- core/android/database/src/main/java/com/vgleadsheets/database/android/UserContentDatabaseVersions.kt
+++ core/android/database/src/main/java/com/vgleadsheets/database/android/UserContentDatabaseVersions.kt
@@ -2,12 +2,6 @@ package com.vgleadsheets.database.android
 
 object UserContentDatabaseVersions {
     const val ORIGINAL = 1
-
-    /**
-     * - Added "offline_song" table.
-     * - Added "offline_game" table.     // TODO
-     * - Added "offline_composer" table.
-     */
     const val ADDED_OFFLINE_SONGS = 2
     const val ADDED_OFFLINE_COMPOSERS = 3
 }
