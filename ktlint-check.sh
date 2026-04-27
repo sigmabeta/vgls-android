49bbe466fe510fae50f0f5b06350d9b8217138b7
sigmabeta <bastos.eder@gmail.com>
2026-04-27 09:21:00 -0400

    Update Ktlint

--- ktlint-check.sh
+++ ktlint-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get a specific version of ktlint
-curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.47.1/ktlint
+curl -sSLO https://github.com/pinterest/ktlint/releases/download/1.8.0/ktlint
 chmod a+x ktlint
 
 # Use it to fix as many problems as it can
-./ktlint --android --reporter=html,output=ktlint.html
+./ktlint --reporter=html,output=ktlint.html
